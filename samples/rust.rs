86248b77f11b9e16f69d85ea2c4eefbdfc559c1b
Pocco81 <pocco451@gmail.com>
2022-03-23 20:28:15 -0500

    updated samples

--- samples/rust.rs
+++ samples/rust.rs
@@ -35,3 +35,13 @@ pub fn every_nth(str: &gsf, n: usize) -> String {
     }
     return parsed_str;
 }
+
+fn main(){
+   let arr:[i32;4] = [10,20,30,40];
+   println!("array is {:?}",arr);
+   println!("array size is :{}",arr.len());
+
+   for index in 0..4 {
+      println!("index is: {} & value is : {}",index,arr[index]);
+   }
+}
