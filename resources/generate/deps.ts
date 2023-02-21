eadbfbe3c74e939254bf4800e56d35c069f58898
winston <hey@winston.sh>
2023-02-21 23:26:24 +0100

    fix(build): use npm import for ajv

--- resources/generate/deps.ts
+++ resources/generate/deps.ts
@@ -1,6 +1,6 @@
 export { parse as parseYaml } from "https://deno.land/std@0.172.0/encoding/yaml.ts";
 
-import Ajv from "https://esm.sh/ajv@8.12.0";
+import Ajv from "npm:ajv@8.12.0";
 import * as path from "https://deno.land/std@0.172.0/path/mod.ts";
 import schema from "../ports.schema.json" assert { type: "json" };
 
