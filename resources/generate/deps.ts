96871c8dcd81754be5874a806cc10649536b6d13
rubyowo <perhaps-you-know@what-is.ml>
2023-03-19 12:58:37 +0400

    docs: point userstyles to the central userstyles repo

--- resources/generate/deps.ts
+++ resources/generate/deps.ts
@@ -2,6 +2,12 @@ export { parse as parseYaml } from "https://deno.land/std@0.172.0/encoding/yaml.
 
 import Ajv from "npm:ajv@8.12.0";
 import * as path from "https://deno.land/std@0.172.0/path/mod.ts";
-import schema from "../ports.schema.json" assert { type: "json" };
+import portsSchema from "../ports.schema.json" assert { type: "json" };
+// TODO: change url when merge
+const userstylesYaml = await fetch(
+  "https://raw.githubusercontent.com/catppuccin-rfc/userstyles/main/src/userstyles.yml",
+);
+import userstylesSchema from "https://raw.githubusercontent.com/catppuccin-rfc/userstyles/main/src/userstyles.schema.json" assert { type: "json" };
+import type { Userstyle, Userstyles } from "https://raw.githubusercontent.com/catppuccin/userstyles/main/src/generate/types.d.ts";
 
-export { Ajv, path, schema };
+export { Ajv, path, portsSchema, userstylesSchema, userstylesYaml, Userstyle, Userstyles };
